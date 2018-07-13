c33df01f107858eb330a659706288ea64348c136
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2018-07-13 19:28:38 -0400

    notes on experiment scaffolding

---
+++ experiments.jl
@@ -0,0 +1,17 @@
+################################################################################
+# module for managing experiments/runs
+################################################################################
+
+# need to keep track of
+# * parameters
+# * data (or hash)
+# * random seed
+# * what kind of experiment
+#
+# to be able to
+# * run a particular experiment
+# * keep track of "batches" (label)
+#
+# really nice if
+# * checkpointing
+# * post-process results
