8308fff166fc7008a32a9f8fafaf58f3c3a4ccc5
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2018-07-21 19:26:39 -0500

    slide generator script

---
+++ watch-slides.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from livereload import Server, shell
+import nbconvert
+
+# from nbconvert.writers import FilesWriter
+# from nbconvert import SlidesExporter
+
+nb_name = 'mathpsych-2018-slides'
+
+exporter = nbconvert.SlidesExporter()
+exporter.reveal_url_prefix = "https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.6.0"
+exporter.reveal_transition = "none"
+# exporter.reveal_theme = "blood"
+
+writer = nbconvert.writers.FilesWriter()
+
+def export_slides():
+    print("regenerating {}".format(nb_name))
+    slides, resources = exporter.from_filename("{}.ipynb".format(nb_name))
+    writer.write(slides, resources, notebook_name=nb_name)
+
+export_slides()
+
+server = Server()
+server.watch('{}.ipynb'.format(nb_name), export_slides, delay=2)
+server.serve()
+
