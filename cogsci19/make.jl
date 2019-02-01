95804b621507c4c3c9304dbfc878bbe5617b4594
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2019-02-01 11:15:45 -0500

    use pdf figures

--- cogsci19/make.jl
+++ cogsci19/make.jl
@@ -7,6 +7,7 @@ using
 function make()
     weave("cogsci.jmd",
           doctype="pandoc",
+          fig_ext=".pdf",
           pandoc_options=["--filter", "pandoc-crossref",
                           "--template", "latexpaper/cogsci_template.tex",
                           "--biblatex", "--pdf-engine=pdflatex"],
