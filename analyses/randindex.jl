2a0548221fff8250eb2af81b8255513751c75e5d
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2019-07-15 15:35:43 -0400

    analysis for adjusted rand index

---
+++ analyses/randindex.jl
@@ -0,0 +1,69 @@
+# compute the Adjusted Rand Index for clusters
+
+using Distributed, Dates
+batch = :randindex
+
+# setup:
+@everywhere begin
+    using Pkg
+    Pkg.activate("..")
+
+    using DataFrames,
+        DataFramesMeta,
+        Underscore,
+        Particles,
+        Distributions,
+        ConjugatePriors,
+        StatsBase,
+        LinearAlgebra
+
+    include("../modeling.jl")
+    include("../experiments.jl")
+    using .Experiments
+    using .DotLearning
+    
+    function ari(params::Dict, data)
+        ps_factory = () -> FearnheadParticles(params[:n],
+                                              params[:prior],
+                                              StickyCRP(params[:α], params[:ρ]))
+        by(data,
+           :subjid1,
+           ari = (:x, :y, :block) => d -> randindex(filter!(ps_factory(),
+                                                            extract_data(d),
+                                                            false),
+                                                    d.block,
+                                                    :adjusted))
+    end
+
+end
+
+
+
+
+using JLD2
+
+# @load "../prior_empirical.jld2"
+# load parameters to get around LinAlg → LinearAlgebra rename
+@load "../prior_empirical_params.jld2"
+prior_optimized = NormalInverseWishart(μ, κ, Λ, ν)
+
+@load "../data/dots2014.jld2"
+
+
+
+expts = experiments(recall,
+                    prior = [prior_optimized],
+                    α = [0.01, 0.1, 1.0, 10.0],
+                    ρ = [0.1, 0.5, 0.9],
+                    n = [100],
+                    batch = [batch],
+                    iter = [1:20;])
+
+
+using .Experiments: add_params!
+
+results = pmap(expts) do ex run(ari, ex) end
+
+results_df = mapreduce(res -> add_params!(res.result, res.experiment.params), vcat, results);
+
+@save "../results/$batch-$(DateTime(now())).jld2" expts results results_df
--- modeling.jl
+++ modeling.jl
@@ -81,9 +81,13 @@ end
 ################################################################################
 # Running on actual data frame:
 
+
+
 # data needs to have columns :x and :y
-extract_data(d::AbstractDataFrame, ps::ParticleFilter) =
-    collect(map(collect, eachslice(hcat(d[:x], d[:y]), dims=1)))
+_vecs(x::Matrix) = collect(map(collect, eachslice(x, dims=1)))
+extract_data(d) = _vecs(hcat(d.x, d.y))
+extract_data(d::AbstractDataFrame, ps::ParticleFilter) = extract_data(d)
+
 
 DataFrames.DataFrame(rf::RecallFilter) =
     DataFrame(x_mod = first.(rf.recalled),
